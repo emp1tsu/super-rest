58129375c72a0f6da4b9b9c95f89ad8ce7a80368
empitsu0813 <oneok0813@gmail.com>
2019-10-16 00:22:39 +0900

    typescriptとmongooseめんどくさすぎる

---
+++ src/models/Users.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema, Document } from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+interface IUser extends Document {
+  name: string
+  email: string
+  password: string
+  tokens: []
+}
+
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: (value: string) => {
+      if (validator.isEmail(value)) return true
+      else return false
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+})
+
+userSchema.pre<IUser>('save', async function(next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+userSchema.methods.generateAuthToken = async function() {
+  const JWT_KEY = process.env.JWT_KEY
+  if (!JWT_KEY) throw new Error('error')
+  const user = this
+  const token = jwt.sign({ _id: user._id }, JWT_KEY)
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+  return token
+}
+
+userSchema.statics.findByCredentials = async (
+  email: string,
+  password: string
+) => {
+  const user = await User.findOne({ email })
+  if (!user) throw new Error('Invalid login credentials')
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password)
+  if (!isPasswordMatch) throw new Error('Invalid login credentials')
+
+  return user
+}
+
+export const User = mongoose.model<IUser>('User', userSchema)
