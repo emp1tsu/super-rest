b78c87fa00f309f14989c2ff6682d6f708b5eef3
empitsu0813 <oneok0813@gmail.com>
2019-10-15 09:44:01 +0900

    add prettier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express'
 // import { userRouter } from "./routers/user";
-require("./db/db");
-const port = process.env.PORT;
+require('./db/db')
+const port = process.env.PORT
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 // app.use(userRouter);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL
 
 try {
-  if (!MONGODB_URL) throw new Error("uris is not defined");
+  if (!MONGODB_URL) throw new Error('uris is not defined')
 
   mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true
-  });
+  })
 } catch (error) {
-  console.error(error);
+  console.error(error)
 }
