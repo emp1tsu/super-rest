aa7480e08152d9e5f1895b10be86220e8eaa4d33
empitsu <oneok0813@gmail.com>
2019-11-04 03:37:38 +0900

    ittann

--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,93 @@ import cors from 'cors'
 import passport from 'passport'
 import errorhandler from 'errorhandler'
 import mongoose from 'mongoose'
+import morgan from 'morgan'
+import methodOverride from 'method-override'
+
+import routers from './routers'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
+interface IError extends Error {
+  status?: number
+}
 const app = express()
 
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(methodOverride())
+app.use(express.static(__dirname + '/public'))
+
+app.use(
+  session({
+    secret: 'conduit',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+)
+
+if (!isProduction) {
+  app.use(errorhandler())
+}
+
 require('./db/db')
-const port = process.env.PORT
+require('./models/Users')
+require('./models/Articles')
+require('./models/Comments')
+require('./config/passport')
 
+app.use(routers)
 
-app.use(express.json())
-// app.use(userRouter);
+// catch 404 & forwart to error handler
+app.use(function(req, res, next) {
+  try {
+    throw new Error('Not Found')
+  } catch (err) {
+    next(err)
+  }
+})
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+if (!isProduction) {
+  app.use(function(
+    err: IError,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    console.log(err.stack)
+
+    res.status(err.status || 500)
+
+    res.json({
+      error: {
+        message: err.message,
+        error: err
+      }
+    })
+  })
+}
+
+app.use(function(
+  err: IError,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  res.status(err.status || 500)
+  res.json({
+    error: {
+      message: err.message,
+      error: {}
+    }
+  })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`)
 })
